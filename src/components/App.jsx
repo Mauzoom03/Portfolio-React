2ed509b67c91ededce69a11d96a5aa8aadaaa1ea
Mauzoom <mauro.quintana@bootcamp-upgrade.com>
2024-04-21 15:47:51 +0200

    Pending responsive Mobile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 import React from "react";
 import Header from "./layouts/Header/Header";
 import Main from "./pages/Main/Main";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route,  } from "react-router-dom";
 import AboutMe from "./pages/AboutMe/AboutMe";
 import Project from "./pages/Project/Project";
 import Footer from "./layouts/Footer/Footer";
@@ -16,18 +16,7 @@ function App() {
       <AboutMe/>
       <Project />
       <Footer />
-=======
-import React from 'react'
-import DesignsController from './DesignsController/DesignsController'
-import Catalogue from './Catalogue/Catalogue'
-
-function App() {
-  return (
-    <div>
-      <Catalogue/>
->>>>>>> 090653e926df2c272e87f81637876a086b27a4f7
     </div>
   );
 }
-
 export default App;
